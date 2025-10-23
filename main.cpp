abbff545ad7ab87c9591db0b57f1c966b510a8fb
Jong Soo Han <jongsoo@g.skku.edu>
2025-10-23 09:21:59 +0900

    Update main window title in main.cpp
    Change the window title to 'Profile Orchestrator (Qt 5)'

--- main.cpp
+++ main.cpp
@@ -1,1 +1,15 @@
+#include "mainwindow.h"
+#include <QApplication>
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    
+    MainWindow w;
+    // 이미지와 유사하게 타이틀 변경
+    w.setWindowTitle("Profile Orchestrator (Qt 5)");
+    w.show();
+    
+    return a.exec();
+}
 
